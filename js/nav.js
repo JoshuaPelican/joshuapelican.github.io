75e397e6168ff3e6e30a94f3146f03e0a2c906c5
JoshuaPelkington <65318134+JoshuaPelican@users.noreply.github.com>
2023-12-13 17:11:21 -0500

    Finished Nav Bar Functionality

--- js/nav.js
+++ js/nav.js
@@ -2,6 +2,39 @@ let activeIndex = 0;
 
 const articles = document.getElementsByClassName("article");
 
+const handleClickDirect = (i) => {
+
+    nextIndex = i;
+
+    const dirIsRight = i > activeIndex;
+    if(dirIsRight){
+        const currentArticle = document.querySelector('[data-index="'+activeIndex+'"]'),
+        nextArticle = document.querySelector('[data-index="'+nextIndex+'"]');
+    
+        currentArticle.dataset.status = "before";
+        nextArticle.dataset.status = "becoming-active-from-before"
+        
+        setTimeout(() =>{
+            nextArticle.dataset.status = "active";
+            activeIndex = nextIndex;
+        });
+    }
+    else{
+        const currentArticle = document.querySelector('[data-index="'+activeIndex+'"]'),
+        nextArticle = document.querySelector('[data-index="'+nextIndex+'"]');
+        
+        currentArticle.dataset.status = "after";
+        nextArticle.dataset.status = "becoming-active-from-after"
+        
+        setTimeout(() =>{
+            nextArticle.dataset.status = "active";
+            activeIndex = nextIndex;
+        });
+    }
+
+
+}
+
 const handleClickLeft = () =>{
     const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : articles.length - 1;
 
