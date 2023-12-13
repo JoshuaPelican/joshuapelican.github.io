91b5e42fe0de8762bca19609c32081487853e2c9
JoshuaPelkington <65318134+JoshuaPelican@users.noreply.github.com>
2023-12-13 16:47:58 -0500

    Functioning Article Transitioning

---
+++ js/nav.js
@@ -0,0 +1,33 @@
+let activeIndex = 0;
+
+const articles = document.getElementsByClassName("article");
+
+const handleClickLeft = () =>{
+    const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : articles.length - 1;
+
+    const currentArticle = document.querySelector('[data-index="'+activeIndex+'"]'),
+    nextArticle = document.querySelector('[data-index="'+nextIndex+'"]');
+
+    currentArticle.dataset.status = "after";
+    nextArticle.dataset.status = "becoming-active-from-after"
+    
+    setTimeout(() =>{
+        nextArticle.dataset.status = "active";
+        activeIndex = nextIndex;
+    });
+}
+
+const handleClickRight = () =>{
+    const nextIndex = activeIndex + 1 <= articles.length - 1 ? activeIndex + 1 : 0;
+
+    const currentArticle = document.querySelector('[data-index="'+activeIndex+'"]'),
+    nextArticle = document.querySelector('[data-index="'+nextIndex+'"]');
+
+    currentArticle.dataset.status = "before";
+    nextArticle.dataset.status = "becoming-active-from-before"
+    
+    setTimeout(() =>{
+        nextArticle.dataset.status = "active";
+        activeIndex = nextIndex;
+    });
+}
